f0fc8283d982b65c45e48321826d640431477cbc
kritish-dhaubanjar <kritish.dhaubanjar@gmail.com>
2021-07-09 01:16:47 +0545

    Added jsonwebtoken & jwt config.

--- server/src/config.js
+++ server/src/config.js
@@ -7,6 +7,13 @@ const config = {
     port: process.env.APP_PORT,
     host: process.env.APP_HOST,
   },
+  jwt: {
+    secret: process.env.JWT_SECRET,
+    option: {
+      expiresIn: process.env.JWT_SECRET_EXPIRE,
+      algorithm: 'HS256',
+    },
+  },
   github: {
     clientID: process.env.GITHUB_CLIENT_ID,
     clientSecret: process.env.GITHUB_CLIENT_SECRET,
