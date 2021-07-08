213a5d24d61b7465759ce6cb0cc6f0ca9a45b97f
kritish-dhaubanjar <kritish.dhaubanjar@gmail.com>
2021-07-09 02:32:07 +0545

    Added generic error handler as middleware.

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,12 @@
 import config from './config';
 import express from 'express';
 import routes from './routes';
+import * as errorHandler from './middlewares/errorHandler';
 
 const app = express();
 
 app.use('/api', routes);
+app.use(errorHandler.genericErrorHandler);
 
 const { port, host } = config.app;
 
