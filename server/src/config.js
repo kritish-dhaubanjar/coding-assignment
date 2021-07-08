7d4b47e5fd05ee3d9f234c22fbeabcf7048cd50e
kritish-dhaubanjar <kritish.dhaubanjar@gmail.com>
2021-07-09 01:02:16 +0545

    Added config for Github OAuth.

--- server/src/config.js
+++ server/src/config.js
@@ -7,6 +7,12 @@ const config = {
     port: process.env.APP_PORT,
     host: process.env.APP_HOST,
   },
+  github: {
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    userURL: process.env.GITHUB_USER_URL,
+    accessTokenURL: process.env.GITHUB_ACCESS_TOKEN_URL,
+  },
 };
 
 export default config;
