b3d2e7f21324658332725acef39d3b6c1e5c6eff
kritish-dhaubanjar <kritish.dhaubanjar@gmail.com>
2021-07-12 13:40:57 +0545

    Multithreading with cluster module added tested on node >= 14.17.3

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,13 @@
+import os from 'os';
 import cors from 'cors';
 import morgan from 'morgan';
 import config from './config';
 import express from 'express';
 import routes from './routes';
+import cluster from 'cluster';
 import * as errorHandler from './middlewares/errorHandler';
+
+const noOfCPUs = os.cpus().length;
 
 const app = express();
 app.use(morgan('combined'));
@@ -15,6 +19,12 @@ app.use(errorHandler.genericErrorHandler);
 
 const { port, host } = config.app;
 
-app.listen(port, host, () => {
-  console.log(`Server started at http://${host}:${port}`);
-});
+if (cluster.isPrimary) {
+  for (let i = 0; i < noOfCPUs; i++) {
+    cluster.fork();
+  }
+} else {
+  app.listen(port, host, () => {
+    console.log(`Server ${process.pid} started at http://${host}:${port}`);
+  });
+}
